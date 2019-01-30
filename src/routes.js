19694814fdc2bab421c0391491d45a94db74d5fd
Da-Woon Chung <da-woon.chung@fjordnet.com>
2019-01-30 14:23:50 +0100

    Push.

--- src/routes.js
+++ src/routes.js
@@ -1,28 +1,19 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import MainTemplate from './templates/Main';
-
 import Home from './pages/Home';
 
 const Routes = () => (
-  <Route
-    path="/"
-    render={() => (
-      <MainTemplate>
-        <Switch>
-          <Route exact path={`${process.env.PUBLIC_URL}/`} component={Home} />
-          <Route
-            render={() => (
-              <div>
-                Nothing found!
-              </div>
-            )}
-          />
-        </Switch>
-      </MainTemplate>
-    )}
-  />
+  <Switch>
+    <Route exact path={`${process.env.PUBLIC_URL}/`} component={Home} />
+    <Route
+      render={() => (
+        <div>
+          Nothing found!
+        </div>
+      )}
+    />
+  </Switch>
 );
 
 export default Routes;
