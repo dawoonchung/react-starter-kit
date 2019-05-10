568c9d0d66ec900deb7167b92ba54195ec93cc74
Da-Woon Chung <da-woon.chung@fjordnet.com>
2019-05-10 11:40:59 +0800

    Remove Redux in favour of Context API.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # An opinionated React starter kit.
 This package was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-It includes basic configurations for React, Redux and React Router.
+It includes basic configurations for React and React Router.
 Additionally, the kit is setup to use Airbnb's eslint guideline as well as strict stylelint.
 
 # TODO
--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import Routes from './routes';
-import configureStore from './store';
 
 import * as serviceWorker from './utilities/serviceWorker';
 
 import './scss/style.scss';
 
-const store = configureStore();
-
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <Routes />
-    </Router>
-  </Provider>,
+  <Router>
+    <Routes />
+  </Router>,
   document.getElementById('root'),
 );
 
