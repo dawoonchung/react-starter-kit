bcfbb62629102716f611855f7065a24750f26e7e
Da-Woon Chung <da-woon.chung@fjordnet.com>
2019-11-13 14:34:28 +0900

    Add grid as default layout.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Home = () => (
   <div className="home">
-    <main>
-      <h1>
+    <main className="container">
+      <h1 className="col-12">
         Start your project!
       </h1>
     </main>
