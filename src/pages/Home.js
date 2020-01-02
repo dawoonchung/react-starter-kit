798b0c6c80ceab4f25090d6f7040a9394459708a
Da-Woon Chung <da-woon.chung@fjordnet.com>
2020-01-02 14:08:34 +0900

    Custom grid system.

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="home">
+    <main className="container-fluid">
+      <div className="row">
+        <h1 className="col-12">
+          Start your project!
+        </h1>
+      </div>
+    </main>
+  </div>
+);
+
+export default Home;
