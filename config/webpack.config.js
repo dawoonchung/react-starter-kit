5b263de6e539a118a3124656a49aa37eb248554a
Da-Woon Chung <dorapen@gmail.com>
2020-06-29 17:42:41 +0200

    Update stylelint configurations.

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -103,7 +103,8 @@ module.exports = function(webpackEnv) {
             // Adds PostCSS Normalize as the reset css with default options,
             // so that it honors browserslist config in package.json
             // which in turn let's users customize the target behavior as per their needs.
-            postcssNormalize(),
+            // postcssNormalize(),
+            require('lost'),
           ],
           sourceMap: isEnvProduction && shouldUseSourceMap,
         },
